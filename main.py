6c174a521a76b432bafe3518a7113975bfde1c09
maia <masarwer@outlook.com>
2024-11-21 17:00:14 -0500

    post
    post

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 
 # use api where the json file is stored for the blog posts
 posts = requests.get("https://api.npoint.io/a31c7e2c5f7dce0e8ecf",  verify=False).json()
-MY_EMAIL = "masarwer@outlook.com"
-MY_PASSWORD = ""
+MY_EMAIL = "maiaaudreysarwer@gmail.com"
+MY_PASSWORD = "jugy ktqv nxly kmnm"
 
 #working for loop will be re-fromatted using Jinja formatting and put into index.html
 # for post in posts:
@@ -23,6 +23,14 @@ MY_PASSWORD = ""
 def  get_all_posts():
     return render_template("index.html", all_posts=posts)
 
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
 @app.route("/about")
 def about():
     return render_template("about.html")
@@ -31,12 +39,18 @@ def about():
 def contact():
     if request.method == "POST":
         data = request.form
-        print(data["name"])
-        print(data["email"])
-        print(data["phone"])
-        print(data["message"])
-        return "<h1>Message sent successfully</h1>"
-    return render_template("contact.html")
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(MY_EMAIL, MY_EMAIL, email_message)
 
 # @app.route("/form-entry", methods=["POST"])
 # def receive_data():
@@ -49,15 +63,15 @@ def contact():
 #     print(phone_input)
 #     return "Successfully sent your message"
 
-@app.route("/post/<int:index>")
-def show_post(index):
-    #set up empty python variable
-    requested_post = None
-    #loop through posts to find the id # that matches the <int:index>
-    for x in posts:
-        if x["id"] == index:
-            requested_post = x
-    return render_template("post.html", post=requested_post)
+# @app.route("/post/<int:index>")
+# def show_post(index):
+#     #set up empty python variable
+#     requested_post = None
+#     #loop through posts to find the id # that matches the <int:index>
+#     for x in posts:
+#         if x["id"] == index:
+#             requested_post = x
+#     return render_template("post.html", post=requested_post)
 
 
 if __name__ == "__main__":
